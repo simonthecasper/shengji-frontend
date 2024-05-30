7d1a21d43ad8d541eef532d828209cc3c1712006
Simon Liu <simon.qy.liu@outlook.com>
2024-05-29 17:08:07 -0700

    testing socketio

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AppServerConnection from "./components/AppServerConnection";
 // import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <AppServerConnection />
     </React.StrictMode>
 );
