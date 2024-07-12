6272feb197972f0253f941a8c167f4a3c9b94242
notin <chadvansyoc11@gmail.com>
2024-07-11 19:16:37 -0700

    css and stuff

--- src/pages/UserLogin.tsx
+++ src/pages/UserLogin.tsx
@@ -3,17 +3,22 @@
 import { useSetAtom } from "jotai";
 import {  nameAndConnectServer  } from "../store/store.ts";
 import { useRef } from "react";
+import '../App.css'
 
 function StartPage() {
     // const [input ] = useAtom(inputAtom)
     const setName = useSetAtom(nameAndConnectServer)
     const input = useRef(null)
 
+    const onSubmit = () =>{
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-expect-error
+        setName(input?.current.value);
+        
+    }
     return <>
-        <form action="">
         <input name="username" ref={input}/>
-        <button onSubmit={ () => setName(input?.current)}></button>
-        </form >
+        <button onClick={ onSubmit} > submit name</button>
     </>;
 }
 
