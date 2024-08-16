6b4d8f5a313dcfec9c9fc77d341987a0d2118836
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-08-15 19:42:33 -0700

    remove unnecessary PageManager middleman

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PageManager from "./pages/PageManager";
+import StartPage from "./pages/StartPage";
 
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
     <React.StrictMode>
-        <PageManager />
+        <StartPage />
     </React.StrictMode>
 );
