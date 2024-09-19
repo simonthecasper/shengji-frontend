550efcf5e967299a058bb82f56707d806bc6d8d3
Simon Liu <simon.qy.liu@outlook.com>
2024-09-18 20:26:40 -0700

    added new style presets to Button and implemented visual switching of the styles in the game selector depending on who the host is and what is selected

--- src/enums/GameTypes.ts
+++ src/enums/GameTypes.ts
@@ -1,8 +1,6 @@
-//enum for 'orginal', 'test2', 'crazy_lvl'
- export enum GameTypes {
-    shengji = "ShengJi",
-    original = 'original',
-    test2 = 'test2',
-    crazy_lvl = 'crazy_lvl'
-}
-
+export const GameTypes = [
+    'ShengJi',
+    'original',
+    'test2',
+    'crazy_lvl'
+]
